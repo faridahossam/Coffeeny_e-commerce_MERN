e540f1175ced1770aaafaaf0bc5ea4ba067adb04
Farida <Faridahossam2889@gmail.com>
2025-07-01 16:56:21 +0300

    Product feature (CRUD)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routers/userRoute";
+import productRouter from "./routers/productRoute";
+import { seedInitialProducts } from "./services/productService";
 
 const app = express();
 const port = 3001;
@@ -11,7 +13,12 @@ mongoose
     .connect("mongodb://localhost:27017/ecommerce")
     .then(() => console.log("Mongo Connected"))
     .catch((err) => console.log("Connection Failed!", err));
+
+// seed the products to database
+seedInitialProducts();
+
 app.use("/user" , userRouter)
+app.use("/product", productRouter);
 
 
 app.listen(port , () =>{
