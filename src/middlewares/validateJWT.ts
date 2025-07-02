11188288e1bf01cf3accc5841fa7d36ba89c2664
Farida <Faridahossam2889@gmail.com>
2025-07-02 16:49:26 +0300

    validate jwt cart

---
+++ src/middlewares/validateJWT.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel";
+
+interface ExtendRequest extends Request {
+  user?: any;
+}
+
+const validateJWT = (req: ExtendRequest, res: Response, next: NextFunction) => {
+  const authorizationHeader = req.get("authorization");
+  if (!authorizationHeader) {
+    res.status(403).send("Authorization header was not provided");
+    return;
+  }
+
+  const token = authorizationHeader.split(" ")[1];
+  if (!token) {
+    res.status(403).send("Bearer token not found");
+    return;
+  }
+  jwt.verify(
+    token,
+    "JyojLmTaombiYW27X8sopov0x8Cc3c8B",
+    async (err, payload) => {
+      if (err) {
+        res.status(403).send("Invalid token");
+        return;
+      }
+
+      if (!payload) {
+        res.status(403).send("Invalid token payload");
+      }
+      const userPayload = payload as {
+        email: string;
+        firstName: string;
+        lastName: string;
+      };
+      //fetch user from data
+      const user = await userModel.findOne({ email: userPayload.email });
+      req.user = user;
+      next();
+    }
+  );
+};
+
+export default validateJWT;
--- src/routers/cartRoute.ts
+++ src/routers/cartRoute.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { getActiveCartForUser } from "../services/cartService";
+import validateJWT from "../middlewares/validateJWT";
 
 const router = express.Router();
 
-router.get('/',async(req,res)=>{
-    //TODO: get the user id from jwt after validating jwt
-    const cart = await getActiveCartForUser({userId: "xxx"})
-    res.status(200).send(cart)
-})
+router.get("/", validateJWT, async (req, res) => {
+  const userId = req.user._id;
+  const cart = await getActiveCartForUser({ userId });
+  res.status(200).send(cart);
+});
 
 export default router;
