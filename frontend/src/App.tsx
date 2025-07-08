2f86885c5d5dce85e358442f55de8896a29a58df
Farida <Faridahossam2889@gmail.com>
2025-07-08 16:13:57 +0300

    mui +navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
+import NavBar from "./components/NavBar"
 
 
 function App() {
 
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/" element={<HomePage />}/>
