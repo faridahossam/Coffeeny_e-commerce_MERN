ae34303df4e213cb1d7e2313f3226243afd6ce02
Farida <Faridahossam2889@gmail.com>
2025-07-08 16:31:01 +0300

    card

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,22 @@
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import ProductCard from "../components/ProductCard";
+
 const HomePage = () => {
-    return <div>Home Page</div>
-}
-export default HomePage
+  return (
+    <Container sx={{ mt: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item md={4}>
+          <ProductCard />
+        </Grid>
+        <Grid item md={4}>
+          <ProductCard />
+        </Grid>
+        <Grid item md={4}>
+          <ProductCard />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+export default HomePage;
